805ff7035d4388e1af979b3c39f462cda64e4cf1
Rafael Lima <drdverzola@gmail.com>
2022-02-15 09:37:26 +0000

    new changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css'
-import MapExample from './Examples/MapExample'
-import NestedArray from './Examples/NestedArray'
+import "./App.css";
+import MapExample from "./Examples/MapExample";
+import NestedArray from "./Examples/NestedArray";
 
 function App() {
-    return (
-        <div className='App'>
-            {/* <MapExample /> */}
-            <NestedArray />
-        </div>
-    )
+  return (
+    <div className="App">
+      <MapExample />
+      {/* <NestedArray /> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Examples/MapExample.jsx
+++ src/Examples/MapExample.jsx
@@ -1,38 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const mockData = [
-    { id: 1, text: 'lorem 1', caption: 'caption_1' },
-    { id: 2, caption: 'caption_2' },
-    { id: 3, text: 'lorem 3' },
-]
+  { id: 1, text: "lorem 1", caption: "caption_1" },
+  { id: 2, caption: "caption_2" },
+  { id: 3, text: "lorem 3" },
+];
 
 function MapExample() {
-    const [serverData, setServerData] = useState([])
-    return (
-        <>
-            <div>
-                {/* {mock response, after fetching data and parsing it} */}
-                <button
-                    onClick={() => {
-                        setServerData(mockData)
-                    }}
-                >
-                    Mock response
-                </button>
-            </div>
+  const [serverData, setServerData] = useState([]);
+  return (
+    <>
+      <div>
+        {/* {mock response, after fetching data and parsing it} */}
+        <button
+          onClick={() => {
+            setServerData(mockData);
+          }}
+        >
+          Mock response
+        </button>
+      </div>
 
+      {serverData.length > 0 ? (
+        <div>
+          {serverData.map((data) => (
             <div>
-                {serverData.map((data) => (
-                    <div>
-                        <h6>{data.id}</h6>
-                        {/* notice that text and imgUrl are doesn't exist in 2 elements of the data */}
-                        <p>{data.text}</p>
-                        <h6>{data.caption}</h6>
-                    </div>
-                ))}
+              <h6>{data.id}</h6>
+              {/* notice that text and imgUrl are doesn't exist in 2 elements of the data */}
+              <p>{data.text}</p>
+              <h6>{data.caption}</h6>
             </div>
-        </>
-    )
+          ))}
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
+  );
 }
 
-export default MapExample
+export default MapExample;
